a712aa57061ccd9ef3912a08047c28ba1f1850e3
Julian <julian.lopezvel@gmail.com>
2024-11-25 18:11:50 -0500

    A derived query to only find active doctors

--- src/main/java/com/challenge/clinicAPI/model/doctor/DoctorRepository.java
+++ src/main/java/com/challenge/clinicAPI/model/doctor/DoctorRepository.java
@@ -1,6 +1,12 @@
 package com.challenge.clinicAPI.model.doctor;
 
+import com.challenge.clinicAPI.model.doctor.dto.DisplayInfoDoctorDTO;
+import com.challenge.clinicAPI.model.doctor.dto.ListDoctorDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+    List<ListDoctorDTO> findByActiveTrue();
 }
