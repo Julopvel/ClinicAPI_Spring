b8cf1b9b604eb0215b725c29510d9778000f8036
Julian <julian.lopezvel@gmail.com>
2024-11-24 22:31:26 -0500

    Address class.

---
+++ src/main/java/com/challenge/clinicAPI/model/Address/Address.java
@@ -0,0 +1,17 @@
+package com.challenge.clinicAPI.model.Address;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String street;
+    private String number;
+    private String city;
+}
