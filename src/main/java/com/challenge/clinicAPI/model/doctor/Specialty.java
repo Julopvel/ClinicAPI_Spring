6c84294abe69fa309eea6e31db1af814f2ff0f5e
Julian <julian.lopezvel@gmail.com>
2024-11-24 22:31:55 -0500

    Doctor's specialties.

---
+++ src/main/java/com/challenge/clinicAPI/model/doctor/Specialty.java
@@ -0,0 +1,13 @@
+package com.challenge.clinicAPI.model.doctor;
+
+public enum Specialty {
+
+    CARDIOLOGY,
+    PEDIATRIC,
+    GYNECOLOGY,
+    UROLOGIST,
+    OPHTHALMOLOGIST,
+    NEUROLOGIST
+
+
+}
