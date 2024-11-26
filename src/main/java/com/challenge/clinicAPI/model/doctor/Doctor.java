1ce31ad0f397ee9bd6480427293ab5ff57e16c77
Julian <julian.lopezvel@gmail.com>
2024-11-26 13:45:02 -0500

    Added the option active, and methods to register and update Doctors information

--- src/main/java/com/challenge/clinicAPI/model/doctor/Doctor.java
+++ src/main/java/com/challenge/clinicAPI/model/doctor/Doctor.java
@@ -1,7 +1,10 @@
 package com.challenge.clinicAPI.model.doctor;
 
-import com.challenge.clinicAPI.model.Address.Address;
+import com.challenge.clinicAPI.model.address.Address;
+import com.challenge.clinicAPI.model.doctor.dto.RegisterDoctorDTO;
+import com.challenge.clinicAPI.model.doctor.dto.UpdateDoctorDTO;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,9 +23,42 @@ public class Doctor {
     private String name;
     private String email;
     private String phone;
-    private String documentId;
+    private String document;
+    private Boolean active;
     @Enumerated(EnumType.STRING)
     private Specialty specialty;
     @Embedded
     private Address address;
+
+    public Doctor(@Valid RegisterDoctorDTO registerDoctorDTO) {
+        this.name = registerDoctorDTO.name();
+        this.email = registerDoctorDTO.email();
+        this.phone = registerDoctorDTO.phone();
+        this.document = registerDoctorDTO.document();
+        this.active = true;
+        this.specialty = registerDoctorDTO.specialty();
+        this.address = new Address(registerDoctorDTO.address());
+    }
+
+    public void updateData(@Valid UpdateDoctorDTO updateDoctorDTO) {
+        if (updateDoctorDTO.name() != null){
+            this.name = updateDoctorDTO.name();
+        }
+        if (updateDoctorDTO.email() != null){
+            this.email = updateDoctorDTO.email();
+        }
+        if (updateDoctorDTO.phone() != null){
+            this.phone = updateDoctorDTO.phone();
+        }
+        if (updateDoctorDTO.document() != null){
+            this.document = updateDoctorDTO.document();
+        }
+//        if (updateDoctorDTO.address() != null){
+//            this.address = new Address(updateDoctorDTO.address());
+//        }
+    }
+
+    public void disableDoctor() {
+        this.active = false;
+    }
 }
