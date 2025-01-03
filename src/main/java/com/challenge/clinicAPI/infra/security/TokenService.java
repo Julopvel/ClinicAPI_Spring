0012e1baa4c05e82d4112cb8f2e4dbdbd54bf233
Julian <julian.lopezvel@gmail.com>
2025-01-03 18:20:12 -0500

    TokenSerivce: method to validate the jwt, from auth0 repo

--- src/main/java/com/challenge/clinicAPI/infra/security/TokenService.java
+++ src/main/java/com/challenge/clinicAPI/infra/security/TokenService.java
@@ -1,8 +1,11 @@
 package com.challenge.clinicAPI.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.challenge.clinicAPI.model.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,10 +20,11 @@ public class TokenService {
     //Esto luego debe ser cambiado a una variable de ambiente para mejorar seguridad.
     @Value("${api.security.secret}")
     private String apiSecret;
+    private DecodedJWT verifier;
 
     public String generateToken(User user){
         try{
-            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret); //Validating token signature
             String token = JWT.create()
                     .withIssuer("clinicAPI")
                     .withSubject(user.getUsername())
@@ -33,6 +37,26 @@ public class TokenService {
         }
     }
 
+    public String getSubject(String token) {
+        verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer("clinicAPI")              // specify any specific claim validations
+                    .build()
+                    .verify(token);
+            verifier.getSubject();  //Subject corresponds to username
+
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null){
+            throw new RuntimeException("Verifier invalid");
+        }
+        return verifier.getSubject();
+    }
+
+    //Here, in this case, the validity of the generated token is up to two hours
     private Instant tokenExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
     }
