a1ab6372b2556efa9377aa03f259d587122549f7
Julian <julian.lopezvel@gmail.com>
2025-01-16 23:01:26 -0500

    Custom exception.

---
+++ src/main/java/com/challenge/clinicAPI/model/ValidityException.java
@@ -0,0 +1,7 @@
+package com.challenge.clinicAPI.model;
+
+public class ValidityException extends RuntimeException {
+    public ValidityException(String message) {
+        super(message);
+    }
+}
