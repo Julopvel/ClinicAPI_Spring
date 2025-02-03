f992d788457953b82ff56e7eb87cf98dec1290ab
Julian <julian.lopezvel@gmail.com>
2025-02-02 23:21:08 -0500

    Improved response within the controller

--- src/main/java/com/challenge/clinicAPI/controller/ConsultController.java
+++ src/main/java/com/challenge/clinicAPI/controller/ConsultController.java
@@ -1,8 +1,10 @@
 package com.challenge.clinicAPI.controller;
 
+import com.challenge.clinicAPI.model.consult.BookingAConsult;
 import com.challenge.clinicAPI.model.consult.dto.DisplayInfoConsultDTO;
 import com.challenge.clinicAPI.model.consult.dto.RegisterConsultDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/consult")
 public class ConsultController {
 
+    private BookingAConsult bookingAConsult;
+
+    @Autowired
+    public ConsultController(BookingAConsult bookingAConsult) {
+        this.bookingAConsult = bookingAConsult;
+    }
+
     @PostMapping
-    public ResponseEntity<DisplayInfoConsultDTO> book (@RequestBody @Valid RegisterConsultDTO data){
-        System.out.println(data);
-        return ResponseEntity.ok(new DisplayInfoConsultDTO(null, null, null, null));
+    public ResponseEntity<DisplayInfoConsultDTO> book (@RequestBody @Valid RegisterConsultDTO registerConsultDTO){
+
+        var detailsConsult = bookingAConsult.book(registerConsultDTO);
+        return ResponseEntity.ok(detailsConsult);
     }
 }
